28a753dfa51d182fca7c394756ac2219b5777034
Thangavel Suyambudurai <praveen.thangavelsuyambudurai@chep.com>
2021-07-26 21:25:57 +1000

    add custom workflow ms dynamics learning

---
+++ StoreWorkFlow/CreateStoreWorkFlow.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.Activities;
+
+namespace StoreWorkFlow
+{
+    public class CreateStoreWorkFlow : CodeActivity
+    {
+
+        [RequiredArgument]
+        [Input("Warehouse Name")]
+        public InArgument<string> StoreName { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var tracer = context.GetExtension<ITracingService>();
+            var factory = context.GetExtension<IOrganizationServiceFactory>();
+            var workflowContext = context.GetExtension<IWorkflowContext>();
+            var service = factory.CreateOrganizationService(workflowContext.UserId);
+            tracer.Trace("Create Warehouse Custom WorkFlow is invoked");
+            var inputStoreName = StoreName.Get(context);
+            var warehouse = new Entity("cr299_aldiwarehouse");
+            warehouse["cr299_name"] = inputStoreName + " Warehouse";
+            warehouse["cr299_location"] = inputStoreName;
+            service.Create(warehouse);
+        }
+    }
+}
